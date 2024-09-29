fba629783906dfd26481ee9e9d034125e3901817
andn15050906 <111441677+andn15050906@users.noreply.github.com>
2024-09-29 16:24:25 +0700

    Hang - Update Layout, Alert and Profile (#11)
    * Apply loading into App.vue, Handle components are located center between header & footer
    
    ---------
    
    Co-authored-by: diehaa <truongthiliem0505@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,63 @@
 <script setup>
-  import { RouterView } from 'vue-router'
-  import HeaderPage from './components/HomeLayout/HeaderPage.vue';
-  import FooterPage from './components/HomeLayout/FooterPage.vue';
+import { RouterView } from 'vue-router'
+import { useRouter } from 'vue-router';
+import { ref, provide, onMounted } from 'vue';
+import HeaderPage from './components/HomeLayout/HeaderPage.vue';
+import FooterPage from './components/HomeLayout/FooterPage.vue';
+import LoadingSpinner from './components/Helper/LoadingSpinner.vue';
+
+const loadingSpinner = ref(null);
+const sweetAlert = ref(null);
+
+const router = useRouter();
+
+provide('loadingSpinner', {
+  showSpinner: () => loadingSpinner.value.showSpinner(),
+  hideSpinner: () => loadingSpinner.value.hideSpinner(),
+});
+
+provide('sweetAlert', {
+  showAlert: (options) => {
+    if (sweetAlert.value && typeof sweetAlert.value.showAlert === 'function') {
+      return sweetAlert.value.showAlert(options);
+    } else {
+      console.error('SweetAlert is not initialized or showAlert is not defined');
+    }
+  }
+});
+
+onMounted(() => {
+  router.beforeEach((to, from, next) => {
+    loadingSpinner.value.showSpinner(); 
+    next();
+  });
+
+  router.afterEach(() => {
+    setTimeout(() => {
+      loadingSpinner.value.hideSpinner(); 
+    }, 300); 
+  });
+});
+
 </script>
 <template>
   <div id="app">
-    <HeaderPage/>
-    <RouterView/>
-    <FooterPage/>
+    <LoadingSpinner ref="loadingSpinner" />
+    <SweetAlert ref="sweetAlert" />
+    <HeaderPage />
+    <main>
+      <RouterView />
+    </main>
+    <FooterPage />
   </div>
 </template>
 
-
+<style scoped>
+main {
+  padding: 0 20px;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/Helper/SweetAlert.vue
@@ -0,0 +1,53 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import Swal from 'sweetalert2';
+
+export default {
+  methods: {
+    showSuccess(message = 'Operation completed successfully!') {
+      Swal.fire({
+        icon: 'success',
+        title: 'Success',
+        text: message,
+        confirmButtonColor: '#3085d6',
+      });
+    },
+
+    showError(message = 'Something went wrong!') {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: message,
+        confirmButtonColor: '#d33',
+      });
+    },
+
+    showWarning(message = 'Are you sure?') {
+      return Swal.fire({
+        title: 'Warning',
+        text: message,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, continue!',
+        cancelButtonText: 'No, cancel!',
+      });
+    },
+
+    showInfo(message = 'This is an information alert') {
+      Swal.fire({
+        icon: 'info',
+        title: 'Info',
+        text: message,
+        confirmButtonColor: '#3085d6',
+      });
+    },
+  }
+};
+</script>
+
+<style scoped></style>
