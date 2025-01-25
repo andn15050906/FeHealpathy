08f0b1e2897b756170057cfce82bf852ffa68552
mai12072003 <127924897+mai12072003@users.noreply.github.com>
2025-01-25 11:19:25 +0700

    update Calendar

--- src/router/index.js
+++ src/router/index.js
@@ -45,6 +45,8 @@ import DailyQuestion from '@/views/QuestionOfTheDay/DailyQuestion.vue'
 import BlogDetail from '@/views/Blogs/BlogDetail.vue'
 import Register from '@/views/Profile/Register.vue'
 import CreateGroupVue from '@/components/GroupComponents/CreateGroup.vue'
+import Calendar from '@/components/Calendar/Calendar.vue' 
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -289,7 +291,12 @@ const router = createRouter({
       path: '/payments',
       name: 'Payments',
       component: PaymentsPage
-    }
+    },
+    {
+      path: '/calendar', 
+      name: 'Calendar',
+      component: Calendar
+    },
   ]
 })
 
